4b6de47d11ed5df4b1d42f2c1981490ff4ada5a9
csiszar.peter <csiszar.peter.95@gmail.com>
2021-04-01 09:15:22 +0200

    Add logout button

--- src/main/java/tk/ynvaser/quiz/security/SecurityConfiguration.java
+++ src/main/java/tk/ynvaser/quiz/security/SecurityConfiguration.java
@@ -1,10 +1,15 @@
 package tk.ynvaser.quiz.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 @EnableWebSecurity
@@ -14,19 +19,35 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final String LOGIN_FAILURE_URL = "/login?error";
     private static final String LOGIN_URL = "/login";
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //Vaadin handles Cross-Site Request Forgery
         http.csrf().disable().anonymous()
+
                 .and().authorizeRequests()
                 .requestMatchers(SecurityUtils::isFrameworkInternalRequest).permitAll()
-
                 .anyRequest().authenticated()
 
                 .and().formLogin().loginPage(LOGIN_URL).permitAll().loginProcessingUrl(LOGIN_URL)
                 .failureUrl(LOGIN_FAILURE_URL)
+                .successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
 
-                .successHandler(new SavedRequestAwareAuthenticationSuccessHandler());
+                .and().logout().logoutSuccessUrl(LOGIN_URL);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder)
+                .withUser("david").password("password").roles("ADMIN");
     }
 
     /**
